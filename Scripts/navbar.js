89355d090e0906e190c35e31e002ca85944708e5
Lucas Martinez <43049088l.m@gmail.com>
2025-05-16 16:16:19 -0300

    Se implementa navbar dinámico

---
+++ Scripts/navbar.js
@@ -0,0 +1,8 @@
+﻿window.addEventListener('scroll', function () {
+    var nav = document.getElementById('navbar');
+    if (window.pageYOffset > 50) {
+        nav.classList.add('shrink');
+    } else {
+        nav.classList.remove('shrink');
+    }
+});
